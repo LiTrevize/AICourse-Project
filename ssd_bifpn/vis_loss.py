4335a0d6956f143fd37ef60f22b50a37b7e1f734
LiTrevize <universeofjimmy@gmail.com>
2019-12-29 14:15:31 +0800

    report almost complete

--- ssd_bifpn/vis_loss.py
+++ ssd_bifpn/vis_loss.py
@@ -17,31 +17,46 @@ parser.add_argument('--plot', action='store_true',
                     help='directory to the output')
 parser.add_argument('--output_dir', default='output/',
                     type=str, help='directory to the output')
-parser.add_argument('-f', action='store_true',
+parser.add_argument('--f', action='store_true',
                     help='force to retrieve data from weight files')
 args = parser.parse_args()
 
 # keywords in args.model
-kws = args.model.split('_')
+
 # maps of iteration to loss
 loss_map = {}
+ALL_KEYS = ['bifpn', 'sr', 'iou', '300', '512']
+INCLUSIVE = [False for i in range(len(ALL_KEYS))]
 
 
 def select_model(x):
-    for kw in kws:
-        if kw not in x:
+    for i, k in enumerate(ALL_KEYS):
+        if (k in x) != INCLUSIVE[i]:
             return False
     return True
 
 
 def get_train_loss_from_ckp():
+    for i, k in enumerate(ALL_KEYS):
+        if k in args.model:
+            INCLUSIVE[i] = True
+
     files = os.listdir(args.weights_dir)
     for w in filter(select_model, files):
         w = w[:len(w) - 4]
         ws = w.split('_')
-        l = float(ws[-1])
-        iter = int(ws[-2])
-        loss_map[iter] = l
+        try:
+            if args.model == 'ssd_bifpn_iou_loss':
+                l_iou = float(ws[-1])
+                l_lc = float(ws[-2])
+                iter = int(ws[-3][4:])
+                loss_map[iter] = (l_lc, l_iou)
+            else:
+                l = float(ws[-1])
+                iter = int(ws[-2])
+                loss_map[iter] = l
+        except:
+            print('invalid', w)
 
 
 if __name__ == '__main__':
@@ -57,12 +72,18 @@ if __name__ == '__main__':
         get_train_loss_from_ckp()
         iter = sorted(loss_map.keys())
         loss = [loss_map[k] for k in iter]
-        train_loss = {'iter': iter, 'loss': loss}
+        train_loss = {'iter': np.array(iter), 'loss': np.array(loss)}
         with open(output_name + '.pkl', 'wb') as f:
             pickle.dump(train_loss, f)
 
     if args.plot:
-        plt.plot(train_loss['iter'], train_loss['loss'])
+        if args.model == 'ssd_bifpn_iou_loss':
+            plt.plot(train_loss['iter'], train_loss['loss'][:, 0], label='loss_loc+loss_conf')
+            plt.plot(train_loss['iter'], train_loss['loss'][:, 1], label='loss_iou')
+            plt.plot(train_loss['iter'], np.sum(train_loss['loss'], axis=1), label='loss_total')
+            plt.legend()
+        else:
+            plt.plot(train_loss['iter'], train_loss['loss'])
         plt.title('training loss of ' + args.model)
         plt.xlabel('iteration')
         plt.ylabel('loss')
